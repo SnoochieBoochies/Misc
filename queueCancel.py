4ad836d5b0bec6f970e06aae3d5fb495cf4fb3f9
SnoochieBoochies <niallmohan@gmail.com>
2014-10-14 12:09:24 +0100

    Modified script a little

--- queueCancel.py
+++ queueCancel.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 ##################################
@@ -9,7 +10,7 @@
 # The script could be easily modified to become some master queued job killer of its dominion, but it's expected to be used in a downstream job for now.
 #################################
 
-import httplib, urllib
+import httplib, urllib, urllib2
 import json
 import ast
 from pprint import pprint 
@@ -18,7 +19,7 @@ import cStringIO
 import sys, getopt
 
 
-url = "http://jenkins.openet.com/queue/api/python?pretty=true"
+url = "<jenkins_url>/queue/api/python?pretty=true"
 jsonAST = ast.literal_eval(urllib.urlopen(url).read())
 #jsonAST = ast.literal_eval(urllib.urlopen('http://10.0.21.218/pk/testQueuePython.out').read())
 #pprint (jsonAST)
@@ -26,6 +27,8 @@ jsonAST = ast.literal_eval(urllib.urlopen(url).read())
 
 jobMap= {}
 
+#If we run this job independently from a dependency flow. Eg: Not a downstream job, we'd want to keep the last item in the queue as that
+#would be the most recent job queued, meaning it is more updated that the previously queued incarnations.
 def buildJobMap():
 	#What we need to kill queued jobs is the job id which is in the id field of the queued job.
 	for item in jsonAST["items"]:
@@ -37,12 +40,11 @@ def buildJobMap():
 			value = [item["id"]]
 			jobMap[item["task"].get("name")] = value
 		else:
-			#we don't want to kill the last duplicate in the list, this is what we want to leave running
-			# And since we're taking from a queue, our list is ordered the way we want	
 			jobMap[item["task"].get("name")].append(item["id"])
 			
 	print jobMap
 	return
+
 
 
 #job_to_monitor is a string of the jenkins job you wish to kill if there's duplicates.
@@ -55,14 +57,13 @@ def doCancel(job_to_monitor):
     for jobKey, jobVal in jobMap.iteritems():
     #Change if statement if we aren't running as a downstream job. At that point we should check whether there is already a job in the queue.
     #only delete when we have more than one id in the list.
+    #In this case, we are a downstream job so we want to delete every occurence of the job since our downstream job is going to kick off.
         if jobKey == job_to_monitor:
-            for i in xrange(0, len(jobVal) - 1):
+            for i in xrange(0, len(jobVal)):
                 print jobVal[i]
+		print "Deleting job ", jobKey, " with id ", jobVal[i]
                 cancelBuild(jobVal[i])
-        elif jobKey != job_to_monitor:
-            print "Could not find job ", job_to_monitor, " in queue. Exiting..."
-            sys.exit()
-
+		
     return
 
 def cancelBuild(jobId):
@@ -74,16 +75,20 @@ def cancelBuild(jobId):
 	curl.setopt(curl.VERBOSE, True)
 	curl.setopt(curl.FAILONERROR, True)
 
-	#convert job int to string with backticks
-	url = "cancelItem?id=" + `jobId`
-	print url
-	curl.setopt(curl.URL, "http://jenkins.openet.com/queue/")
-	curl.setopt(curl.POSTFIELDS, url)
+	values = {'id': `jobId`}
+	
+	#html = urllib.urlopen(url, urllib.urlencode(values)).read()
+	#print html
+	#print url
+	values = urllib.urlencode(values)
+	curl.setopt(curl.URL, "<jenkins_url>/queue/cancelItem?")
+	curl.setopt(curl.POST, 1)
+	curl.setopt(curl.POSTFIELDS, values)
 
 	try:
 	    curl.perform()
 
-	    print buf.getvalue()
+	    #print buf.getvalue()
 	    
 	except pycurl.error, error:
 	    errno, errstr = error
