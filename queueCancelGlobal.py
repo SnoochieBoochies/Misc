ae7c3dbbfe19e06ad2a488c38ac5f1847ff0f759
SnoochieBoochies <niallmohan@gmail.com>
2015-01-12 15:20:27 +0000

    Adding in some rough stats with pyplot and numpy

--- queueCancelGlobal.py
+++ queueCancelGlobal.py
@@ -22,7 +22,9 @@ import os
 import os.path
 import csv
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 
 from optparse import OptionParser
@@ -34,6 +36,8 @@ jsonAST = ast.literal_eval(urllib.urlopen(url).read())
 #print type(jsonAST["items"])
 
 jobMap= {}
+
+statsDirectory = "JobStats"
 
 #If we run this job independently from a dependency flow. Eg: Not a downstream job, we'd want to keep the last item in the queue as that
 #would be the most recent job queued, meaning it is more updated that the previously queued incarnations.
@@ -84,11 +88,13 @@ def doCancel(job_to_monitor):
                 #print jobMap
                 # TODO trying to add job deletion statistics in a graph. Must remember to truncate the
                 # stats file so it doesn't get fricking huge over time.
-                write_stat(dt.datetime.now(), 1)
-                #cancelBuild(jobVal[i])
+                #print dt.datetime.now().strftime(('%Y-%m-%d %H:%M:%S'))
+                write_stat(jobKey, dt.datetime.now().strftime(('%Y-%m-%d')), 1)
+                cancelBuild(jobVal[i])
 
             # else add a 0 to the stats file indicating we didn't delete anything
-            write_stat(dt.datetime.now(), 0)
+            #print dt.datetime.now().strftime(('%Y-%m-%d %H:%M:%S'))
+            write_stat(jobKey, dt.datetime.now().strftime(('%Y-%m-%d')), 0)
 
     return
 
@@ -124,19 +130,123 @@ def cancelBuild(jobId):
     buf.close()
     return
 
-def write_stat(timestamp, deleted):
-    stats_file = open("stats.stats", "a")
+#write to a file per job.
+def write_stat(jobName, timestamp, deleted):
+    if not os.path.exists(statsDirectory):
+        os.makedirs(statsDirectory)
+
+    stats_file = open(statsDirectory + "/" + jobName + "_stats.stats", "a")
     file_path = os.path.abspath(stats_file.name)
+    #print file_path
     file_modified = dt.datetime.fromtimestamp(os.path.getmtime(file_path))
     # Delete after a week's logging.
     if dt.datetime.now() - file_modified > dt.timedelta(hours=168):
         os.remove(file_path)
 
     csv_writer = csv.writer(stats_file)
-    data = [deleted, timestamp]
+    data = [timestamp, deleted]
+
     csv_writer.writerow(data)
 
     stats_file.close()
+    return
+
+
+def genGraphs():
+    for statsFile in os.listdir(statsDirectory):
+        print statsFile
+        
+        filePath = statsDirectory + "/" + statsFile
+        print filePath
+
+        #There are 14 bytes in a file with one line with our csv format with a carriage return + line feed.
+        #Only graph files with atleast 2 lines of data. Numpy/pyplot doesn't like 1 line of input
+        if os.stat(filePath).st_size > 14:
+            #impressions are either 1 or 0, indicating whether a job was killed in that run
+            days, impressions = np.loadtxt(filePath,unpack=True,delimiter=",", converters={0: mdates.strpdate2num('%Y-%m-%d')})
+            
+            print days
+            #print impressions
+            #convert numbers to ints so we can sum them up, giving us our maximum on the y-axis
+            totalList = [0]
+            totalList = totalList + map(int, impressions)
+            print "totalList = ",totalList
+            
+            #Remove duplicate entries from days list
+            daysFinal = list(set(days))
+            #print daysFinal
+
+            listOfTotalsPerDay = [0]*len(daysFinal)
+            print "listOftotalsPerDay = ", listOfTotalsPerDay
+            daysInts = map(int, daysFinal)
+            print "daysInts = ", daysInts
+            daysOldInts = map(int, days)
+            print "daysOldInts = ",daysOldInts
+
+            
+            daysOldMap = {}
+            
+            counter = 0
+            for day in daysOldInts:
+                daySum = 1
+                #print totalList[counter]
+                if day not in daysOldMap:
+                    daysOldMap[day] = totalList[counter]
+                else:
+                    daysOldMap[day] = daysOldMap[day] + totalList[counter]
+                counter+=1
+            print "daysOldMap = ", daysOldMap
+            
+            
+            maxY = sum(totalList)
+            print maxY
+            print daysOldMap.keys()
+            print daysOldMap.values()
+
+                   
+
+            if maxY == 0:
+                plt.axhline(y=0)
+            else:
+                axes = plt.gca()
+                axes.set_ylim([0,maxY])
+
+
+            #Format date of x-axis
+            fmt = mdates.DateFormatter('%Y-%m-%d')
+            loc = mdates.WeekdayLocator(byweekday=mdates.MONDAY)
+            ax = plt.axes()
+            ax.xaxis.set_major_formatter(fmt)
+            ax.xaxis.set_major_locator(loc) 
+
+            
+            #Do the plot.
+            plt.grid(True)
+            plt.plot_date(x=daysOldMap.keys(), y=daysOldMap.values(), fmt='ro-')
+
+            
+            fig = plt.figure(1)
+            fig.autofmt_xdate()
+
+
+            #Give a title to the graph
+            jobName = statsFile[:-6]
+            graphTitle = "Frequency of killed queued jobs for " + jobName
+            plt.title(graphTitle)
+            plt.ylabel("No. of times job was killed")
+
+
+            #N = 3
+            #params = plt.gcf()
+            #pltSize = params.get_size_inches()
+            #params.set_size_inches( (pltSize[0]*N, pltSize[1]*N) )
+
+            plt.show()
+            
+            #Save it.
+            pngName = jobName + ".png"
+            plt.savefig(pngName) 
+            
     return
 
 def main(argv):
@@ -164,13 +274,8 @@ def main(argv):
     elif options.job_to_monitor == '':
         print "one job to rule them all flow"
         doCancel('')
-        #plot our stats and save to png
-        data = np.genfromtxt("stats.stats", delimiter=',', dtype=None)
-        print data
-        plt.plotfile("stats.stats",(0,))
-        plt.savefig('test1.png')
-    #print jobMap
-
+        genGraphs()
+               
  
 if __name__ == "__main__":
     main(sys.argv[1:])
